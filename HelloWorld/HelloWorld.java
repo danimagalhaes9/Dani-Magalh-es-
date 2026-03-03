28124bece8785e8c32f1c9d074f71afe4f394202
danimagalhaes9 <danielmagalhaes20177@icloud.com>
2026-03-03 19:22:00 -0300

    Add files via upload

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld {
+  public static void main (String[]args) {
+
+  
+
+String nome = "Dani"; 
+int idade = 23;
+double altura = (1.74);
+System.out.println (nome);
+System.out.println (idade);
+System.out.println (altura);
+
+
+} 
+
+
+}
+
+
+
